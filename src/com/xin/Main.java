33165f5deafd4bdc75b91cbb53fcd86c7897e28a
Xin <xinae4321@hotmail.com>
2019-10-13 22:51:38 +0800

    initial

--- src/com/xin/Main.java
+++ src/com/xin/Main.java
@@ -8,11 +8,23 @@ public class Main {
 
         int incrementingNumber = 1;
 
-        System.out.print("Enter value: ");
+        System.out.print("Enter string: ");
         Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String reverse = "";
+        for (int i = x.length() - 1; i >= 0; i--){
+            reverse = reverse + x.charAt(i);
+        }
+
+        if (x.equals(reverse)){
+            System.out.println(x + " is Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        System.out.print("Enter a number: ");
         int test = sc.nextInt();
-        System.out.println("Enter string: ");
-        String pal = sc.nextLine();
+
 
         for (int i = 1; i <= test; i++){
             for (int j = 1; j <= i; j++) {
